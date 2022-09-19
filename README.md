ca28937644ad24c5276ac2860940815ca31da45d
Zoltán Gyebrovszki <kavics@sensenet.com>
2022-09-19 03:01:28 +0200

    Initial.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SnAuthDemo
+Simple .NET 6 console application to demonstrate the sensenet's authentication feature.
+The app connects to your SNaaS repository by clientId-cleindSecret pair and gets the names of the root's children.
+
+## Steps for personalization:
+1. Configure the app: change the `sensenet/Url` value to yours in the *appSettings.json*.
+2. Change the ClientId and ClientSecret in *appSettings.json* or in your user secret.
+3. If you use the user secret, ensure that the `AddUserSecrets` method's parameter in the *Program.cs* line 9 equals to `UserSecretsId` in the *SnAuthDemo.csproj*. If not, copy the value from the csproj and paste it into the code.
---
+++ src/SnAuthDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SenseNet.Client;
+using SenseNet.Extensions.DependencyInjection;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets("1587edec-e6f2-487b-b724-9145275cc73f")
+    .Build();
+
+// assemble a service container and register the sensenet repository service
+var provider = new ServiceCollection()
+    .AddSenseNetRepository(options => { config.Bind("sensenet", options); })
+    .AddLogging(logging => logging.AddConsole())
+    .BuildServiceProvider();
+
+var serverFactory = provider.GetRequiredService<IServerContextFactory>();
+var server = await serverFactory.GetServerAsync();
+
+var children = await Content.LoadCollectionAsync("/Root", server);
+Console.WriteLine("Children of /Root");
+foreach (var content in children)
+    Console.WriteLine($"  {content.Name,-20} {content["Type"]}");
